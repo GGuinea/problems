8ec84d2b7f60d15314221c478ac58bebee5161ae
Kamil Zielinski <kamil5176@gmail.com>
2022-07-16 13:03:26 +0200

    bitd_count

--- exercism/elixir/bird-count/lib/bird_count.ex
+++ exercism/elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,28 @@
 defmodule BirdCount do
-  def today([head | _tail]) do
-    head 
+  def today([head | _]) do
+    head
   end
 
-  def increment_day_count([head | _tail]) do
-    [head + 1 | _tail]
+  def today([]) do
+    nil
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def increment_day_count([]) do
+    [1]
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def increment_day_count([head | tail]) do
+    [head + 1 | tail]
   end
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | tail]), do: has_day_without_birds?(tail)
+
+  def total([]), do: 0
+  def total([head | tail]), do: head + total(tail)
+
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5, do: 1 + busy_days(tail)
+  def busy_days([_ | tail]), do: busy_days(tail)
 end
