f5d3c9a3764578663c5f051d7dc87b15a1c65341
gguinea <kamil5176@gmail.com>
2023-11-16 21:59:12 +0100

    d5p1

---
+++ adventOfCode/2022/go/day5/solution.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func readFile(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(1)
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	lines := make([]string, 0)
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func part1() {
+	lines := readFile("input")
+	stacks := make(map[int][]string)
+	for _, line := range lines {
+		if strings.Contains(line, "move") {
+			break
+		}
+		parseStacks(line, stacks)
+	}
+	for _, line := range lines {
+		if !strings.Contains(line, "move") {
+			continue
+		}
+		from, to, count := parseMoves(line)
+		makeMove(from, to, count, stacks)
+	}
+	var res string
+	fmt.Println(stacks)
+	for key, _ := range stacks {
+		fmt.Println(key)
+	}
+	for i :=1; i < len(stacks)+1; i++ {
+		fmt.Println(stacks[i])
+		res += strings.Join(stacks[i][:1], "")
+	}
+	fmt.Println(res)
+}
+
+func parseStacks(line string, stacks map[int][]string) {
+	stackPointer := 0
+	for i := 0; i < len(line); i++ {
+		if i%4 == 0 {
+			stackPointer++
+		}
+
+		character := line[i]
+		if character >= 'A' && character <= 'Z' {
+			stacks[stackPointer] = append(stacks[stackPointer], string(character))
+		}
+	}
+}
+
+func makeMove(from, to, count int, stacks map[int][]string) {
+	movedObjects := make([]string, count)
+	copy(movedObjects, stacks[from][:count])
+	slices.Reverse(movedObjects)
+	stacks[to] = append(movedObjects, stacks[to]...)
+	stacks[from] = stacks[from][count:]
+}
+
+func parseMoves(line string) (int, int, int) {
+	line = strings.Replace(line, "move ", "", 1)
+	line = strings.Replace(line, " from ", ",", 1)
+	line = strings.Replace(line, " to ", ",", 1)
+	splitted := strings.Split(line, ",")
+	count, _ := strconv.Atoi(splitted[0])
+	from, _ := strconv.Atoi(splitted[1])
+	to, _ := strconv.Atoi(splitted[2])
+	return from, to, count
+}
+
+func main() {
+	part1()
+}
