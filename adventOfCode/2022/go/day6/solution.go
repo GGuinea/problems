111aa11713baa2bcb0769909cd60cbea98a595df
gguinea <kamil5176@gmail.com>
2023-11-17 11:01:44 +0100

    d6p1

---
+++ adventOfCode/2022/go/day6/solution.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readFile(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(1)
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	lines := make([]string, 0)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
+}
+
+func part1() {
+	lines := readFile("input")
+	res := findUniqueSlidingWindow(lines[0], 4)
+	fmt.Println(res)
+}
+
+func findUniqueSlidingWindow(line string, size int) int {
+	splitted := strings.Split(line, "")
+	for i := 0; i < len(line); i++ {
+		if isUnique(splitted[i : i+size]) {
+			return i + size
+		}
+	}
+	return -1
+}
+
+func isUnique(window []string) bool {
+	occuranceMap := make(map[string]int)
+	for _, lett := range window {
+		_, ok := occuranceMap[lett]
+		if ok {
+			return false
+		}
+		occuranceMap[lett] = 1
+	}
+	return true
+}
+
+func main() {
+	part1()
+}
