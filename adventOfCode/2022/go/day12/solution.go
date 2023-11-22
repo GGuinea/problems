09bee6df4ea95e59fc8edee350d1f3f55458aa1f
gguinea <kamil5176@gmail.com>
2023-11-22 16:51:58 +0100

    d12p1

---
+++ adventOfCode/2022/go/day12/solution.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+const S int = 83
+const E int = 69
+const VISITED_MARKER int = -1
+
+type Maze struct {
+	Labyrinth       [][]int
+	Found           bool
+	ShorthestLength int
+	Length          int
+	Visited         [][]int
+	Queue           []Point
+	Start           Point
+	End             Point
+}
+
+type Point struct {
+	X    int
+	Y    int
+	dist int
+}
+
+func enqueue(queue []Point, element Point) []Point {
+	return append(queue, element)
+}
+
+func dequeue(queue []Point) (Point, []Point) {
+	element := queue[0]
+	if len(queue) == 1 {
+		var tmp = []Point{}
+		return element, tmp
+	}
+	return element, queue[1:]
+}
+
+func readFile(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(1)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	lines := make([]string, 0)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
+}
+
+func part1(fileName string) {
+	lines := readFile(fileName)
+	labyrinth, x, y, x2, y2 := parseInput(lines)
+
+	queue := []Point{}
+	queue = enqueue(queue, Point{X: x, Y: y, dist: 0})
+	visited := make([][]int, len(labyrinth))
+
+	end := Point{X: x2, Y: y2}
+	start := Point{X: x, Y: y, dist: 0}
+
+	for i := range visited {
+		visited[i] = make([]int, len(labyrinth[0]))
+
+	}
+
+	maze := Maze{
+		Labyrinth:       labyrinth,
+		Found:           false,
+		ShorthestLength: math.MaxInt,
+		Visited:         visited,
+		Queue:           queue,
+		Start:           start,
+		End:             end,
+	}
+
+	for len(maze.Queue) > 0 {
+		point, queue := dequeue(maze.Queue)
+		maze.Queue = queue
+
+		if point.X == maze.End.X && point.Y == maze.End.Y {
+			fmt.Println(point.dist)
+			return
+		}
+
+		if maze.Visited[point.X][point.Y] == VISITED_MARKER {
+			continue
+		}
+
+		maze.Visited[point.X][point.Y] = VISITED_MARKER
+
+		if canVisit(&maze, point.X+1, point.Y, maze.Labyrinth[point.X][point.Y]) {
+			maze.Queue = enqueue(maze.Queue, Point{X: point.X + 1, Y: point.Y, dist: point.dist + 1})
+		}
+
+		if canVisit(&maze, point.X, point.Y+1, maze.Labyrinth[point.X][point.Y]) {
+			maze.Queue = enqueue(maze.Queue, Point{X: point.X, Y: point.Y + 1, dist: point.dist + 1})
+		}
+
+		if canVisit(&maze, point.X-1, point.Y, maze.Labyrinth[point.X][point.Y]) {
+			maze.Queue = enqueue(maze.Queue, Point{X: point.X - 1, Y: point.Y, dist: point.dist + 1})
+		}
+
+		if canVisit(&maze, point.X, point.Y-1, maze.Labyrinth[point.X][point.Y]) {
+			maze.Queue = enqueue(maze.Queue, Point{X: point.X, Y: point.Y - 1, dist: point.dist + 1})
+		}
+	}
+}
+
+func canVisit(maze *Maze, x, y int, currentValue int) bool {
+	if x < 0 || y < 0 || x >= len(maze.Labyrinth) || y >= len(maze.Labyrinth[0]) {
+		return false
+	}
+
+	if maze.Visited[x][y] == VISITED_MARKER {
+		return false
+	}
+
+	if (maze.Labyrinth[x][y] - currentValue) > 1 {
+		return false
+	}
+
+	return true
+}
+
+func getAbsDifference(a, b int) int {
+	return int(math.Abs(float64(a) - float64(b)))
+}
+
+func parseInput(lines []string) ([][]int, int, int, int, int) {
+	labyrinth := make([][]int, 0)
+	startingI := 0
+	startingJ := 0
+
+	endI := 0
+	endJ := 0
+
+	for i, line := range lines {
+		arr := make([]int, 0)
+		for j, lett := range line {
+			if int(lett) == S {
+				startingI = i
+				startingJ = j
+				arr = append(arr, 97)
+			} else if int(lett) == E {
+				endI = i
+				endJ = j
+				arr = append(arr, 122)
+			} else {
+				arr = append(arr, int(lett))
+			}
+		}
+		labyrinth = append(labyrinth, arr)
+	}
+
+	return labyrinth, startingI, startingJ, endI, endJ
+}
+
+func main() {
+	fileName := os.Args[1]
+	part1(fileName)
+}
