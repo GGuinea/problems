6b056c738b5564afc88203ac4cbd0218760b5004
gguinea <kamil5176@gmail.com>
2023-12-12 11:44:01 +0100

    d5p2 - does not work with normal input :(

--- adventOfCode/2023/go/day5/solution.go
+++ adventOfCode/2023/go/day5/solution.go
@@ -25,6 +25,11 @@ func (rs *RangeStruct) getDestination(x int) (bool, int) {
 	return false, x
 }
 
+func (rs *RangeStruct) getDestination2(x int) int {
+	diff := x - rs.SourceStart
+	return rs.Destination + diff
+}
+
 func readFile(fileName string) []string {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -58,6 +63,31 @@ func parseSeeds(line string) []int {
 	}
 
 	return seeds
+}
+
+func parseSeedsPart2(line string) []RangeStruct {
+	seedsRanges := make([]int, 0)
+
+	line, _ = strings.CutPrefix(line, "seeds: ")
+	split := strings.Split(line, " ")
+
+	for _, num := range split {
+		converted, err := strconv.Atoi(num)
+		if err != nil {
+			panic(2)
+		}
+		seedsRanges = append(seedsRanges, converted)
+	}
+
+	ranges := make([]RangeStruct, 0)
+
+	for i := 0; i < len(seedsRanges); i += 2 {
+		start := seedsRanges[i]
+		end := start + seedsRanges[i+1]
+		ranges = append(ranges, RangeStruct{SourceStart: start, SourceEnd: end})
+	}
+
+	return ranges
 }
 
 func parseRanges(lines []string) map[int][]RangeStruct {
@@ -105,7 +135,6 @@ func convertNumbers(numbersAsStrings []string) RangeStruct {
 func part1(filename string) int {
 	lines := readFile(filename)
 	seeds := parseSeeds(lines[0])
-	fmt.Println(seeds)
 	parsedRanges := parseRanges(lines[3:])
 
 	for i := 0; i < len(parsedRanges); i++ {
@@ -129,7 +158,85 @@ func part1(filename string) int {
 	return slices.Min(seeds)
 }
 
+func part2(filename string) int {
+	lines := readFile(filename)
+	seedsRanges := parseSeedsPart2(lines[0])
+	parsedRanges := parseRanges(lines[3:])
+
+	for i := 0; i < len(parsedRanges); i++ {
+		seedsRanges = applyRange(seedsRanges, parsedRanges[i])
+	}
+
+	mins := make([]int, 0)
+
+	for _, seedRange := range seedsRanges {
+		if seedRange.SourceStart == 0 {
+			continue
+		}
+		mins = append(mins, seedRange.SourceStart)
+	}
+	return slices.Min(mins)
+}
+
+func applyRange(seedsRanges []RangeStruct, mapRanges []RangeStruct) []RangeStruct {
+	newSeedRanges := make([]RangeStruct, 0)
+
+	for _, seedRange := range seedsRanges {
+		mapped := false
+		for _, mapRange := range mapRanges {
+			if seedRange.SourceStart >= mapRange.SourceStart && seedRange.SourceEnd <= mapRange.SourceEnd {
+				newSeedRanges = myAppend(newSeedRanges, RangeStruct{SourceStart: mapRange.getDestination2(seedRange.SourceStart), SourceEnd: mapRange.getDestination2(seedRange.SourceEnd)})
+				mapped = true
+				break
+			}
+
+			if seedRange.SourceStart <= mapRange.SourceStart && seedRange.SourceEnd >= mapRange.SourceEnd {
+				beforeCommonRange := RangeStruct{SourceStart: seedRange.SourceStart, SourceEnd: mapRange.SourceStart}
+				commonRange := RangeStruct{SourceStart: mapRange.getDestination2(mapRange.SourceStart), SourceEnd: mapRange.getDestination2(mapRange.SourceEnd)}
+				afterCommonRange := RangeStruct{SourceStart: mapRange.SourceEnd, SourceEnd: seedRange.SourceEnd}
+				newSeedRanges = myAppend(newSeedRanges, beforeCommonRange)
+				newSeedRanges = myAppend(newSeedRanges, commonRange)
+				newSeedRanges = myAppend(newSeedRanges, afterCommonRange)
+				mapped = true
+				break
+			}
+
+			if seedRange.SourceStart < mapRange.SourceStart && seedRange.SourceEnd < mapRange.SourceEnd && seedRange.SourceEnd >= mapRange.SourceStart {
+				beforeCommonRange := RangeStruct{SourceStart: seedRange.SourceStart, SourceEnd: mapRange.SourceStart}
+				commonRange := RangeStruct{SourceStart: mapRange.getDestination2(mapRange.SourceStart), SourceEnd: mapRange.getDestination2(seedRange.SourceEnd)}
+				newSeedRanges = myAppend(newSeedRanges, beforeCommonRange)
+				newSeedRanges = myAppend(newSeedRanges, commonRange)
+				mapped = true
+				break
+			}
+
+			if seedRange.SourceStart >= mapRange.SourceStart && seedRange.SourceEnd >= mapRange.SourceStart && mapRange.SourceEnd >= seedRange.SourceStart {
+				commonRange := RangeStruct{SourceStart: mapRange.getDestination2(seedRange.SourceStart), SourceEnd: mapRange.getDestination2(mapRange.SourceEnd)}
+				afterCommonRange := RangeStruct{SourceStart: mapRange.SourceEnd, SourceEnd: seedRange.SourceEnd}
+				newSeedRanges = myAppend(newSeedRanges, commonRange)
+				newSeedRanges = myAppend(newSeedRanges, afterCommonRange)
+				mapped = true
+				break
+			}
+		}
+			if !mapped {
+				newSeedRanges = append(newSeedRanges, seedRange)
+			}
+
+	}
+
+	return newSeedRanges
+}
+
+func myAppend(destination []RangeStruct, source RangeStruct) []RangeStruct {
+	if source.SourceStart == source.SourceEnd {
+		return append(destination, RangeStruct{SourceStart: source.SourceStart, SourceEnd: source.SourceEnd + 1})
+	}
+	return append(destination, source)
+}
+
 func main() {
 	fileName := os.Args[1]
-	fmt.Println(part1(fileName))
+	//fmt.Println(part1(fileName))
+	fmt.Println(part2(fileName))
 }
--- adventOfCode/2023/go/day5/solution_test.go
+++ adventOfCode/2023/go/day5/solution_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -81,22 +83,489 @@ func TestShouldCompletePart1WithProperInput(t *testing.T) {
 	}
 }
 
-// func TestShouldCompletePart2WithTestInput(t *testing.T) {
-// 	testFileName := "test_input"
-// 	expected := 30
-// 	res := part2(testFileName)
-//
-// 	if res != expected {
-// 		t.Fatalf("Wrong number, should be %d, current: %d", expected, res)
-// 	}
-// }
-//
-// func TestShouldCompletePart2WithProperInput(t *testing.T) {
-// 	testFileName := "input"
-// 	expected := 6420979
-// 	res := part2(testFileName)
-//
-// 	if res != expected {
-// 		t.Fatalf("Wrong number, should be %d, current: %d", expected, res)
-// 	}
-// }
+func TestParseSeedsPart2(t *testing.T) {
+	inputLine := "seeds: 79 14 55 13"
+	seeds := parseSeedsPart2(inputLine)
+	fmt.Println(seeds)
+	if len(seeds) != 27 {
+		t.Fatalf("Should be 27 seeds, current: %d", len(seeds))
+	}
+}
+
+func TestApplyRangesSeedRangeIsOverlapedByMapRange(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 3,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 17,
+			SourceEnd:   27,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeIsOverlapedByMapRange2(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   2,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   10,
+			Destination: 1,
+			Range:       9,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   2,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeIsOverlapedByMapRange3(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   20,
+			Destination: 1,
+			Range:       19,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeOverlapingMapRange(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   40,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 3,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   3,
+		},
+		{
+			SourceStart: 10,
+			SourceEnd:   37,
+		},
+		{
+			SourceStart: 30,
+			SourceEnd:   40,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeOverlapingMapRange2(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   40,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   3,
+		},
+		{
+			SourceStart: 10,
+			SourceEnd:   38,
+		},
+		{
+			SourceStart: 30,
+			SourceEnd:   40,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeOverlapingMapRange3(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   40,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 30,
+			SourceEnd:   40,
+			Destination: 1,
+			Range:       10,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 2,
+			SourceEnd:   30,
+		},
+		{
+			SourceStart: 1,
+			SourceEnd:   11,
+		},
+		{
+			SourceStart: 40,
+			SourceEnd:   41,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeHasCommonBeginningWithMapRange(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   10,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 3,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   3,
+		},
+		{
+			SourceStart: 10,
+			SourceEnd:   17,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeHasCommonBeginningWithMapRange2(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   10,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+			Destination: 1,
+			Range:       10,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   10,
+		},
+		{
+			SourceStart: 1,
+			SourceEnd:   2,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeHasCommonEndWithMapRange(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 20,
+			SourceEnd:   40,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 3,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 27,
+			SourceEnd:   37,
+		},
+		{
+			SourceStart: 30,
+			SourceEnd:   40,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangesSeedRangeHasCommonEndWithMapRange2(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 20,
+			SourceEnd:   30,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   20,
+			Destination: 1,
+			Range:       19,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 20,
+			SourceEnd:   21,
+		},
+		{
+			SourceStart: 20,
+			SourceEnd:   30,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangeSeedAndMapRangeDoesNotHaveCommonPart(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   10,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 20,
+			SourceEnd:   30,
+			Destination: 10,
+			Range:       27,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 0,
+			SourceEnd:   10,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestApplyRangeSeedAndMapRangeDoesNotHaveCommonPart2(t *testing.T) {
+	seedsRanges := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+			Destination: 0,
+			Range:       0,
+		},
+	}
+
+	mapRanges := []RangeStruct{
+		{
+			SourceStart: 1,
+			SourceEnd:   9,
+			Destination: 10,
+			Range:       8,
+		},
+	}
+
+	expected := []RangeStruct{
+		{
+			SourceStart: 10,
+			SourceEnd:   20,
+		},
+	}
+
+	newSeedRanges := applyRange(seedsRanges, mapRanges)
+	res := reflect.DeepEqual(newSeedRanges, expected)
+
+	if res != true {
+		fmt.Println(newSeedRanges)
+		t.Fatalf("Should be equal!")
+	}
+}
+
+func TestShouldCompletePart2WithTestInput(t *testing.T) {
+	testFileName := "test_input"
+	expected := 46
+	res := part2(testFileName)
+
+	if res != expected {
+		t.Fatalf("Wrong number, should be %d, current: %d", expected, res)
+	}
+}
+
+func TestShouldCompletePart2WithProperInput(t *testing.T) {
+	testFileName := "input"
+	expected := 52510809
+	res := part2(testFileName)
+
+	if res != expected {
+		t.Fatalf("Wrong number, should be %d, current: %d", expected, res)
+	}
+}
